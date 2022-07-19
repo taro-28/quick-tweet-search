d62d8ade2ff8b9462e51779c183895407be8c04f
Taro Ohnishi <taro@taro.local>
2022-07-19 23:37:59 +0900

    headerの色を変更

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,22 @@
 import { Box, Center, Heading } from "@chakra-ui/react";
 import { FC, memo } from "react";
-import { FaSearchengin } from "react-icons/fa";
+import { FaSearch } from "react-icons/fa";
 
 export const Header: FC = memo(() => {
   return (
     <Center>
-      <Heading size="md" as="h1" display="flex" mb="2" alignItems="center">
-        <Box mr="1">
-          <FaSearchengin />
+      <Heading
+        size="md"
+        as="h1"
+        display="flex"
+        mb="2"
+        alignItems="center"
+        color="#00acee"
+      >
+        <Box>
+          <FaSearch color="#00acee" />
         </Box>
-        Quick Tweet Search
-        <Box ml="1">
-          <FaSearchengin />
-        </Box>
+        uick Tweet Search
       </Heading>
     </Center>
   );
